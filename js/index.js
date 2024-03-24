79a0e706a87723633edb0a02311f8857cc937de9
APOgamer <adrianpalmaobi23@gmail.com>
2024-03-23 23:36:25 -0500

    landing 2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        loop: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper(".swiper-container", {
+        pagination: {
+            el: ".swiper-pagination",
         },
+        slidesPerView: 1, // Solo se muestra una imagen a la vez
+        loop: true, // Bucle infinito
+        autoplay: {
+            delay: 5000, // Tiempo de espera entre imágenes en milisegundos
+            disableOnInteraction: false, // No detener la reproducción al interactuar con el slider
+        },
+        speed: 1000, // Velocidad de transición en milisegundos (1 segundo)
     });
 });
