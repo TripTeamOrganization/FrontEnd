fe1cefcb9d9c7a7b044603fc0a4116b81654e558
Leonel-Alfaro <u20201a930@upc.edu.pe>
2024-04-11 23:39:22 -0500

    fix: add resgistro

--- js/registro.js
+++ js/registro.js
@@ -4,7 +4,7 @@ document.querySelector('#boton-menu').addEventListener('click', () => {
 });
 
 function validarFormulario() {
-    // Obtener los valores de los campos
+
     var nombre = document.getElementById("nombre").value;
     var apellidos = document.getElementById("apellidos").value;
     var dni = document.getElementById("dni").value;
@@ -13,7 +13,7 @@ function validarFormulario() {
     var telefono = document.getElementById("telefono").value;
     var errores = 0;
 
-    // Limpiar mensajes de error previos
+
     document.getElementById("nombreError").style.display = "none";
     document.getElementById("apellidosError").style.display = "none";
     document.getElementById("dniError").style.display = "none";
@@ -21,48 +21,48 @@ function validarFormulario() {
     document.getElementById("passwordError").style.display = "none";
     document.getElementById("telefonoError").style.display = "none";
 
-    // Validar nombre (entre 3 y 15 caracteres)
+
     if (nombre.length < 3 || nombre.length > 15) {
         document.getElementById("nombreError").style.display = "block";
         errores = 1;
     }
 
-    // Validar apellidos (entre 3 y 15 caracteres)
+
     if (apellidos.length < 3 || apellidos.length > 15) {
         document.getElementById("apellidosError").style.display = "block";
         errores = 1;
     }
 
-    // Validar DNI (8 dígitos numéricos)
+
     var dniRegex = /^\d{8}$/;
     if (!dni.match(dniRegex)) {
         document.getElementById("dniError").style.display = "block";
         errores = 1;
     }
 
-    // Validar correo electrónico (formato básico)
+
     var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!email.match(emailRegex)) {
         document.getElementById("emailError").style.display = "block";
         errores = 1;
     }
 
-    // Validar contraseña (mínimo 8 caracteres)
+
     if (password.length < 8) {
         document.getElementById("passwordError").style.display = "block";
         errores = 1;
     }
 
-    // Validar número de teléfono (opcional)
+
     if (telefono !== "") {
-        var telefonoRegex = /^\d{9}$/; // Ejemplo: 9 dígitos numéricos
+        var telefonoRegex = /^\d{9}$/;
         if (!telefono.match(telefonoRegex)) {
             document.getElementById("telefonoError").style.display = "block";
             errores = 1;
         }
     }
 
-    // Si no hay errores, hacer un console.log
+
     if (errores === 0) {
         const userData = {
             nombre: nombre,
@@ -77,4 +77,4 @@ function validarFormulario() {
     }
 }
 
-// Resto del código...
+
